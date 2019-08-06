d6398de04e0f3c556f73f54a7b7f32631cb51a39
Dmitriy Kalinin <cppforlife@gmail.com>
2019-08-06 16:25:18 -0700

    Update install.sh

--- pkg/website/templates/install.sh
+++ pkg/website/templates/install.sh
@@ -16,8 +16,8 @@ install() {
 	echo "Installed ytt"
 
 	echo "Installing kbld..."
-	wget -O- https://github.com/k14s/kbld/releases/download/v0.10.0/kbld-linux-amd64 > /tmp/kbld
-	echo "56a0ba05431d3e5e4da0da710ba0b7ce98a7f848c15509359da2e58d451cae72  /tmp/kbld" | shasum -c -
+	wget -O- https://github.com/k14s/kbld/releases/download/v0.11.0/kbld-linux-amd64 > /tmp/kbld
+	echo "1029110ffa0263fb75a9deb25642f577bb9d5ee25b1a5b10db55310e05388569  /tmp/kbld" | shasum -c -
 	mv /tmp/kbld /usr/local/bin/kbld
 	chmod +x /usr/local/bin/kbld
 	echo "Installed kbld"
