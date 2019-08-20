b898c1169b54fb5b26c73d29545b1e4f3ac60692
Dmitriy Kalinin <dkalinin@pivotal.io>
2019-08-20 15:48:16 -0700

    add K14SIO_INSTALL_BIN_DIR env var for install.sh

--- pkg/website/templates/install.sh
+++ pkg/website/templates/install.sh
@@ -8,33 +8,35 @@ fi
 install() {
 	set -euo pipefail
 
+	dst_dir="${K14SIO_INSTALL_BIN_DIR:-/usr/local/bin}"
+
 	echo "Installing ytt..."
 	wget -O- https://github.com/k14s/ytt/releases/download/v0.18.0/ytt-linux-amd64 > /tmp/ytt
 	echo "86fdeb00432079209ddec7e2a780f8c8d3faa71dd9f87aed439bb134f0dcfb10  /tmp/ytt" | shasum -c -
-	mv /tmp/ytt /usr/local/bin/ytt
-	chmod +x /usr/local/bin/ytt
-	echo "Installed ytt"
+	mv /tmp/ytt ${dst_dir}/ytt
+	chmod +x ${dst_dir}/ytt
+	echo "Installed ${dst_dir}/ytt"
 
 	echo "Installing kbld..."
 	wget -O- https://github.com/k14s/kbld/releases/download/v0.11.0/kbld-linux-amd64 > /tmp/kbld
 	echo "1029110ffa0263fb75a9deb25642f577bb9d5ee25b1a5b10db55310e05388569  /tmp/kbld" | shasum -c -
-	mv /tmp/kbld /usr/local/bin/kbld
-	chmod +x /usr/local/bin/kbld
-	echo "Installed kbld"
+	mv /tmp/kbld ${dst_dir}/kbld
+	chmod +x ${dst_dir}/kbld
+	echo "Installed ${dst_dir}/kbld"
 
 	echo "Installing kapp..."
 	wget -O- https://github.com/k14s/kapp/releases/download/v0.11.0/kapp-linux-amd64 > /tmp/kapp
 	echo "1d461864afeef5b78ac9eaed7be5acdd699be82f907e7c7efecbbe00b041fbb5  /tmp/kapp" | shasum -c -
-	mv /tmp/kapp /usr/local/bin/kapp
-	chmod +x /usr/local/bin/kapp
-	echo "Installed kapp"
+	mv /tmp/kapp ${dst_dir}/kapp
+	chmod +x ${dst_dir}/kapp
+	echo "Installed ${dst_dir}/kapp"
 
 	echo "Installing kwt..."
 	wget -O- https://github.com/k14s/kwt/releases/download/v0.0.5/kwt-linux-amd64 > /tmp/kwt
 	echo "706abe487e38c4f673180600d11098e408e6bc22fefb1cc512e3ac0f07a9072c  /tmp/kwt" | shasum -c -
-	mv /tmp/kwt /usr/local/bin/kwt
-	chmod +x /usr/local/bin/kwt
-	echo "Installed kwt"
+	mv /tmp/kwt ${dst_dir}/kwt
+	chmod +x ${dst_dir}/kwt
+	echo "Installed ${dst_dir}/kwt"
 }
 
 install
