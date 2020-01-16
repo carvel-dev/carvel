af07abfcf35703f501cc5efa8526d6d17b817a59
Dmitriy Kalinin <cppforlife@gmail.com>
2020-01-16 17:20:29 -0500

    support installing binaries via install.sh on macos

--- hack/test.sh
+++ hack/test.sh
@@ -4,4 +4,9 @@ set -e -x -u
 
 bash -n ./pkg/website/templates/install.sh
 
+export K14SIO_INSTALL_BIN_DIR=./tmp
+
+PATH=./hack/fake-path-darwin/:$PATH ./pkg/website/templates/install.sh
+PATH=./hack/fake-path-linux/:$PATH ./pkg/website/templates/install.sh
+
 echo SUCCESS
--- pkg/website/templates/install.sh
+++ pkg/website/templates/install.sh
@@ -5,47 +5,72 @@ if test -z "$BASH_VERSION"; then
   exit 1
 fi
 
-if [[ x`uname` == xDarwin ]]; then
-  echo "This script is only supported on Linux"
-  exit 1
-fi
-
 install() {
 	set -euo pipefail
 
 	dst_dir="${K14SIO_INSTALL_BIN_DIR:-/usr/local/bin}"
 
+	if [ -x "$(command -v wget)" ]; then
+		dl_bin="wget -nv -O-"
+	else
+		dl_bin="curl -s -L"
+	fi
+
+	ytt_version=v0.24.0
+	kbld_version=v0.13.0
+	kapp_version=v0.17.0
+	kwt_version=v0.0.6
+	imgpkg_version=v0.1.0
+
+	if [[ x`uname` == xDarwin ]]; then
+	  binary_type=darwin-amd64
+	  ytt_checksum=dad9e162745fe2e394b3b8c798182357e5cb4caedba57d2ce0aafda6c6520418
+		kbld_checksum=62d3201b31e7f78ae3d6c1d0621aeb21a59be94bd990811e8af28b61f9ec86c1
+		kapp_checksum=49dbf96dbed4c72774e13dadd2211f77bbad91c2044fa0eae5401ccd0082887f
+		kwt_checksum=555d50d5bed601c2e91f7444b3f44fdc424d721d7da72955725a97f3860e2517
+		imgpkg_checksum=39f1925e39cec7f5837c06c8fce3499a4a24aace9612b8cb15d3835cef4222a0
+	else
+		binary_type=linux-amd64
+		ytt_checksum=c3f1d4f04108ac1626c9b9036c7d4e407d4ff09f2577953ad72b6dc7adadbd39
+		kbld_checksum=c5dc9a5e2fc1795f64f674cbc528a28c269432ce9485ee4dc74d8d18890dd4be
+		kapp_checksum=696abb7e53d047d9d606da8df5965141ab5ee1a97fc06ceeddcca9fc9531520a
+		kwt_checksum=92a1f18be6a8dca15b7537f4cc666713b556630c20c9246b335931a9379196a0
+		imgpkg_checksum=a9d0ba0edaa792d0aaab2af812fda85ca31eca81079505a8a5705e8ee1d8be93
+	fi
+
+	echo "Installing ${binary_type} binaries..."
+
 	echo "Installing ytt..."
-	wget -O- https://github.com/k14s/ytt/releases/download/v0.24.0/ytt-linux-amd64 > /tmp/ytt
-	echo "c3f1d4f04108ac1626c9b9036c7d4e407d4ff09f2577953ad72b6dc7adadbd39  /tmp/ytt" | shasum -c -
+	$dl_bin https://github.com/k14s/ytt/releases/download/${ytt_version}/ytt-${binary_type} > /tmp/ytt
+	echo "${ytt_checksum}  /tmp/ytt" | shasum -c -
 	mv /tmp/ytt ${dst_dir}/ytt
 	chmod +x ${dst_dir}/ytt
 	echo "Installed ${dst_dir}/ytt"
 
 	echo "Installing kbld..."
-	wget -O- https://github.com/k14s/kbld/releases/download/v0.13.0/kbld-linux-amd64 > /tmp/kbld
-	echo "c5dc9a5e2fc1795f64f674cbc528a28c269432ce9485ee4dc74d8d18890dd4be  /tmp/kbld" | shasum -c -
+	$dl_bin https://github.com/k14s/kbld/releases/download/${kbld_version}/kbld-${binary_type} > /tmp/kbld
+	echo "${kbld_checksum}  /tmp/kbld" | shasum -c -
 	mv /tmp/kbld ${dst_dir}/kbld
 	chmod +x ${dst_dir}/kbld
 	echo "Installed ${dst_dir}/kbld"
 
 	echo "Installing kapp..."
-	wget -O- https://github.com/k14s/kapp/releases/download/v0.17.0/kapp-linux-amd64 > /tmp/kapp
-	echo "696abb7e53d047d9d606da8df5965141ab5ee1a97fc06ceeddcca9fc9531520a  /tmp/kapp" | shasum -c -
+	$dl_bin https://github.com/k14s/kapp/releases/download/${kapp_version}/kapp-${binary_type} > /tmp/kapp
+	echo "${kapp_checksum}  /tmp/kapp" | shasum -c -
 	mv /tmp/kapp ${dst_dir}/kapp
 	chmod +x ${dst_dir}/kapp
 	echo "Installed ${dst_dir}/kapp"
 
 	echo "Installing kwt..."
-	wget -O- https://github.com/k14s/kwt/releases/download/v0.0.6/kwt-linux-amd64 > /tmp/kwt
-	echo "92a1f18be6a8dca15b7537f4cc666713b556630c20c9246b335931a9379196a0  /tmp/kwt" | shasum -c -
+	$dl_bin https://github.com/k14s/kwt/releases/download/${kwt_version}/kwt-${binary_type} > /tmp/kwt
+	echo "${kwt_checksum}  /tmp/kwt" | shasum -c -
 	mv /tmp/kwt ${dst_dir}/kwt
 	chmod +x ${dst_dir}/kwt
 	echo "Installed ${dst_dir}/kwt"
 
 	echo "Installing imgpkg..."
-	wget -O- https://github.com/k14s/imgpkg/releases/download/v0.1.0/imgpkg-linux-amd64 > /tmp/imgpkg
-	echo "a9d0ba0edaa792d0aaab2af812fda85ca31eca81079505a8a5705e8ee1d8be93  /tmp/imgpkg" | shasum -c -
+	$dl_bin https://github.com/k14s/imgpkg/releases/download/${imgpkg_version}/imgpkg-${binary_type} > /tmp/imgpkg
+	echo "${imgpkg_checksum}  /tmp/imgpkg" | shasum -c -
 	mv /tmp/imgpkg ${dst_dir}/imgpkg
 	chmod +x ${dst_dir}/imgpkg
 	echo "Installed ${dst_dir}/imgpkg"
