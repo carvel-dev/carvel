2a261e8fa73bc6f2342bb96a9f4e1010eba1cada
Dmitriy Kalinin <dkalinin@vmware.com>
2020-10-26 21:24:29 -0400

    bump kbld

--- pkg/website/templates/install.sh
+++ pkg/website/templates/install.sh
@@ -19,7 +19,7 @@ install() {
   shasum -v 1>/dev/null 2>&1 || (echo "Missing shasum binary" && exit 1)
 
   ytt_version=v0.30.0
-  kbld_version=v0.26.0
+  kbld_version=v0.27.0
   kapp_version=v0.34.0
   kwt_version=v0.0.6
   imgpkg_version=v0.2.0
@@ -28,7 +28,7 @@ install() {
   if [[ x`uname` == xDarwin ]]; then
     binary_type=darwin-amd64
     ytt_checksum=a1a56c3292e355b9891b2c4ce7525d78f0e1ffd8630b856d300e9a7f383e707c
-    kbld_checksum=65ac1a4b108c1478bf53883bfec492577d80cf950f90a6e9331b202dbff4bad5
+    kbld_checksum=17e1336b714f9b6e9a99f86c6a8952be56b018d7aacab712f70e59430edfbdab
     kapp_checksum=61795970d69c530c134711e35fca35ef143176c9a32bf8dce9ef39b1bd0d3b75
     kwt_checksum=555d50d5bed601c2e91f7444b3f44fdc424d721d7da72955725a97f3860e2517
     imgpkg_checksum=e37f55e1dbd3ace7daf6ab8356c11f8104af1601f92ba96eebc57aa74c18cfa4
@@ -36,7 +36,7 @@ install() {
   else
     binary_type=linux-amd64
     ytt_checksum=456e58c70aef5cd4946d29ed106c2b2acbb4d0d5e99129e526ecb4a859a36145
-    kbld_checksum=63d35026696ffa23ea4dbf52d47cda02e1844a538fead30bf2ea78ea92410c21
+    kbld_checksum=9c2c38ce2f884523a6888c3ba5c28bb9a7ab5d3f9879ab8492db1372e206e83b
     kapp_checksum=e170193c40ff5dff9f9274c25048de1f50e23c69e8406df274fbb416d5862d7f
     kwt_checksum=92a1f18be6a8dca15b7537f4cc666713b556630c20c9246b335931a9379196a0
     imgpkg_checksum=57a73c4721c39f815408f486c1acfb720af82450996e2bfdf4c2c280d8a28dcc
