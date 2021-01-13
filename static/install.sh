b5056f2c5e08174fbc42a9e624d83c2868a5ff25
gcheadle-vmware <67297250+gcheadle-vmware@users.noreply.github.com>
2021-01-13 13:56:03 -0800

    Bump kbld version to v0.28.0

--- static/install.sh
+++ static/install.sh
@@ -19,7 +19,7 @@ install() {
   shasum -v 1>/dev/null 2>&1 || (echo "Missing shasum binary" && exit 1)
 
   ytt_version=v0.31.0
-  kbld_version=v0.27.0
+  kbld_version=v0.28.0
   kapp_version=v0.34.0
   kwt_version=v0.0.6
   imgpkg_version=v0.3.0
@@ -28,7 +28,7 @@ install() {
   if [[ `uname` == Darwin ]]; then
     binary_type=darwin-amd64
     ytt_checksum=bf85381a12954793d5a18f252579cb1bc043aada2c34ac0974e2ab8cf03a1de3
-    kbld_checksum=17e1336b714f9b6e9a99f86c6a8952be56b018d7aacab712f70e59430edfbdab
+    kbld_checksum=4962a56a755fb3f12678d1024007bcf87485e350b031924d5f24c3361e7dd7f9
     kapp_checksum=61795970d69c530c134711e35fca35ef143176c9a32bf8dce9ef39b1bd0d3b75
     kwt_checksum=555d50d5bed601c2e91f7444b3f44fdc424d721d7da72955725a97f3860e2517
     imgpkg_checksum=05bd8815c049b61fce0935a59149f0daca06ea62526b3961a33b3608fe90835e
@@ -36,7 +36,7 @@ install() {
   else
     binary_type=linux-amd64
     ytt_checksum=32e7cdc38202b49fe673442bd22cb2b130e13f0f05ce724222a06522d7618395
-    kbld_checksum=9c2c38ce2f884523a6888c3ba5c28bb9a7ab5d3f9879ab8492db1372e206e83b
+    kbld_checksum=3174e5b42286aab4359d2bc12a85356ba520744fd4e26d6511fb8d705e7170c3
     kapp_checksum=e170193c40ff5dff9f9274c25048de1f50e23c69e8406df274fbb416d5862d7f
     kwt_checksum=92a1f18be6a8dca15b7537f4cc666713b556630c20c9246b335931a9379196a0
     imgpkg_checksum=7ebd513bdb4d448764725202f0bfe41e052a594eddeb55e53681ebdf4c27d4dc
