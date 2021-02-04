b948441b9feadb927bd45097aa06f4d47d156fd0
Dennis Leon <leonde@vmware.com>
2021-02-04 13:52:03 -0800

    Bump imgpkg and kbld versions
    Authored-by: Dennis Leon <leonde@vmware.com>

--- static/install.sh
+++ static/install.sh
@@ -19,27 +19,27 @@ install() {
   shasum -v 1>/dev/null 2>&1 || (echo "Missing shasum binary" && exit 1)
 
   ytt_version=v0.31.0
-  kbld_version=v0.28.0
+  kbld_version=v0.29.0
   kapp_version=v0.35.0
   kwt_version=v0.0.6
-  imgpkg_version=v0.3.0
+  imgpkg_version=v0.4.0
   vendir_version=v0.16.0
 
   if [[ `uname` == Darwin ]]; then
     binary_type=darwin-amd64
     ytt_checksum=bf85381a12954793d5a18f252579cb1bc043aada2c34ac0974e2ab8cf03a1de3
-    kbld_checksum=4962a56a755fb3f12678d1024007bcf87485e350b031924d5f24c3361e7dd7f9
+    kbld_checksum=3754fa1ba3a26368aef32769bfb084dd62e52be9c5af0b65e574729e92d21cbc
     kapp_checksum=2f7b37c9cf5a8fd46e7b5da181c93ac3c16fba21664e3c5e413e38fca3510cc2
     kwt_checksum=555d50d5bed601c2e91f7444b3f44fdc424d721d7da72955725a97f3860e2517
-    imgpkg_checksum=05bd8815c049b61fce0935a59149f0daca06ea62526b3961a33b3608fe90835e
+    imgpkg_checksum=5d751e00ca67e0fde17d2ea9d5ed8f32a344a13690efccc88edd33fb6687d2b0
     vendir_checksum=3e6af7ae5cd89579f6d153af6b6a4c0ab1cfcac22f5014b983d1d942feb8bab0
   else
     binary_type=linux-amd64
     ytt_checksum=32e7cdc38202b49fe673442bd22cb2b130e13f0f05ce724222a06522d7618395
-    kbld_checksum=3174e5b42286aab4359d2bc12a85356ba520744fd4e26d6511fb8d705e7170c3
+    kbld_checksum=28492a398854e8fec7dd9537243b07af7f43e6598e1e4557312f5481f6840499
     kapp_checksum=0f9d4daa8c833a8e245362c77e72f4ed06d4f0a12eed6c09813c87a992201676
     kwt_checksum=92a1f18be6a8dca15b7537f4cc666713b556630c20c9246b335931a9379196a0
-    imgpkg_checksum=7ebd513bdb4d448764725202f0bfe41e052a594eddeb55e53681ebdf4c27d4dc
+    imgpkg_checksum=68f6798af67e62dc7ac738162db9e29ce428d41245d56c88fc041ed435376d49
     vendir_checksum=05cede475c2b947772a9fe552380927054d48158959c530122a150a93bf542dd
   fi
 
