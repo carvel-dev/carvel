5c87d3d38b9c9dc2fae8f3dfa77e92724fecfa01
João Pereira <joaod@vmware.com>
2021-02-23 10:25:44 -0500

    Update imgpkg version to v0.5.0

--- static/install.sh
+++ static/install.sh
@@ -22,7 +22,7 @@ install() {
   kbld_version=v0.29.0
   kapp_version=v0.35.0
   kwt_version=v0.0.6
-  imgpkg_version=v0.4.0
+  imgpkg_version=v0.5.0
   vendir_version=v0.16.0
 
   if [[ `uname` == Darwin ]]; then
@@ -31,7 +31,7 @@ install() {
     kbld_checksum=3754fa1ba3a26368aef32769bfb084dd62e52be9c5af0b65e574729e92d21cbc
     kapp_checksum=2f7b37c9cf5a8fd46e7b5da181c93ac3c16fba21664e3c5e413e38fca3510cc2
     kwt_checksum=555d50d5bed601c2e91f7444b3f44fdc424d721d7da72955725a97f3860e2517
-    imgpkg_checksum=5d751e00ca67e0fde17d2ea9d5ed8f32a344a13690efccc88edd33fb6687d2b0
+    imgpkg_checksum=465d0ef98eeca0d5487ad0372703d5d72c7989dc301b9ae6aa934fcb8e20b73b
     vendir_checksum=3e6af7ae5cd89579f6d153af6b6a4c0ab1cfcac22f5014b983d1d942feb8bab0
   else
     binary_type=linux-amd64
@@ -39,7 +39,7 @@ install() {
     kbld_checksum=28492a398854e8fec7dd9537243b07af7f43e6598e1e4557312f5481f6840499
     kapp_checksum=0f9d4daa8c833a8e245362c77e72f4ed06d4f0a12eed6c09813c87a992201676
     kwt_checksum=92a1f18be6a8dca15b7537f4cc666713b556630c20c9246b335931a9379196a0
-    imgpkg_checksum=68f6798af67e62dc7ac738162db9e29ce428d41245d56c88fc041ed435376d49
+    imgpkg_checksum=90dff1d3d88d51a1f73687f9239265afd8bc5275c9f3bc51e4d8f2f381b01f41
     vendir_checksum=05cede475c2b947772a9fe552380927054d48158959c530122a150a93bf542dd
   fi
 
