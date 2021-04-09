b867bf3a59bdd8e1665957ed93e20b1890b7879c
Cari Dean <cdean@pivotal.io>
2021-04-09 14:42:51 -0700

    Simplest go function
    Signed-off-by: Steven Locke <slocke@vmware.com>

--- netlify/functions/test-go/main.go
+++ netlify/functions/test-go/main.go
@@ -5,23 +5,12 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-lambda-go/lambdacontext"
 )
 
-func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	lc, ok := lambdacontext.FromContext(ctx)
-	if !ok {
-		return &events.APIGatewayProxyResponse{
-			StatusCode: 503,
-			Body:       "Something went wrong :(",
-		}, nil
-	}
-
-	cc := lc.ClientContext
-
+func handler(_ context.Context, _ events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello, " + cc.Client.AppTitle,
+		Body:       "Hello world.",
 	}, nil
 }
 
