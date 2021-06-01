29e39670e1e5aa0a21fbd5809d38acc405520e0a
Dmitriy Kalinin <cppforlife@gmail.com>
2021-06-01 12:04:39 -0400

    Bump kapp to 0.37.0

--- static/install.sh
+++ static/install.sh
@@ -20,7 +20,7 @@ install() {
 
   ytt_version=v0.33.0
   kbld_version=v0.30.0
-  kapp_version=v0.36.0
+  kapp_version=v0.37.0
   kwt_version=v0.0.6
   imgpkg_version=v0.8.0
   vendir_version=v0.16.0
@@ -29,7 +29,7 @@ install() {
     binary_type=darwin-amd64
     ytt_checksum=7783d2b8a2087b18179674685dcac0de463cdc50269c676dbd365c590114d0ba
     kbld_checksum=73274d02b0c2837d897c463f820f2c8192e8c3f63fd90c526de5f23d4c6bdec4
-    kapp_checksum=1076da52c7291a7b9c7c0169b59fa18ef5248dee1e060676f8f5af325826d508
+    kapp_checksum=da6411b79c66138cd7437beb268675edf2df3c0a4a8be07fb140dd4ebde758c1
     kwt_checksum=555d50d5bed601c2e91f7444b3f44fdc424d721d7da72955725a97f3860e2517
     imgpkg_checksum=11439505cec7cd0c28182cdb2e269368be8c51376fdd57846289a4ad81460fde
     vendir_checksum=3e6af7ae5cd89579f6d153af6b6a4c0ab1cfcac22f5014b983d1d942feb8bab0
@@ -37,7 +37,7 @@ install() {
     binary_type=linux-amd64
     ytt_checksum=de20cb812f6c5f66feeb66cfcc82607d21c2b726cb54cfd0eaddd112f062e5ca
     kbld_checksum=76c5c572e7a9095256b4c3ae2e076c370ef70ce9ff4eb138662f56828889a00c
-    kapp_checksum=22fe308f1d9ebbb829a6ea10ca80d9468ff4b9aa911b0c33788fe67d04ccb383
+    kapp_checksum=f845233deb6c87feac7c82d9b3f5e03ced9a4672abb1a14d4e5b74fe53bc4538
     kwt_checksum=92a1f18be6a8dca15b7537f4cc666713b556630c20c9246b335931a9379196a0
     imgpkg_checksum=d998c1628c5956ffc84b36f23bec6fd0145977e76f3c02a0c28962e6f8f233c2
     vendir_checksum=05cede475c2b947772a9fe552380927054d48158959c530122a150a93bf542dd
