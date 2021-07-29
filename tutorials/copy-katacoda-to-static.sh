c3b16076dff0bc9dbc6a563e8f2361921d922f00
Joe Kimmel <jkimmel@vmware.com>
2021-07-29 11:46:13 -0700

    playground link and update script

--- tutorials/copy-katacoda-to-static.sh
+++ tutorials/copy-katacoda-to-static.sh
@@ -1,19 +1,21 @@
 #!/bin/zsh
 
-cat > combo.md << EOF
+export DESTFILE="$(dirname $0)/../site/content/kapp-controller/docs/latest/packaging-tutorial.md"
+
+cat > $DESTFILE << EOF
 ## Package Management Tutorial
 
 ### Get Started With Katacoda
 Make a katacoda account and take our interactive tutorial [here](https://katacoda.com/carvel/scenarios/kapp-controller-package-management)
 ### Or Follow Our Tutorial Below
-You can spin up your favorite playground and follow the steps below.
+You can spin up your favorite [playground](https://www.katacoda.com/courses/kubernetes/playground) and follow the steps below.
 Note the below steps are from the linked katacoda tutorial so your environment may differ slightly.
 
 EOF
 
 
 for file in `find $(dirname $0)/katacoda/kapp-controller-package-management/*md | grep -v step01 | grep -v intro | sort`; do
-    # echo $file | grep -v intro | grep -v finish | sed 's/.*step\([0-9]*\).*/##### Step \1/' >> combo.md
-    cat $file | sed 's/{{.*}}//' | sed 's/^## /### /' | sed 's/^# /## /' | >> combo.md
+    # echo $file | grep -v intro | grep -v finish | sed 's/.*step\([0-9]*\).*/##### Step \1/' >> $DESTFILE
+    cat $file | sed 's/{{.*}}//' | sed 's/^## /### /' | sed 's/^# /## /' | >> $DESTFILE
 done
 
