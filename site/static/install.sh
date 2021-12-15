8fa95a3817532228c0d7eafa4225c3033c1e6370
k8slt <klt@groups.vmware.com>
2021-12-15 20:26:40 +0000

    chore: autopublish 2021-12-15T20:26:40Z

--- site/static/install.sh
+++ site/static/install.sh
@@ -19,7 +19,7 @@ install() {
   shasum -v 1>/dev/null 2>&1 || (echo "Missing shasum binary" && exit 1)
 
   ytt_version=v0.38.0
-  kbld_version=v0.31.0
+  kbld_version=v0.32.0
   kapp_version=v0.43.0
   kwt_version=v0.0.6
   imgpkg_version=v0.24.0
@@ -28,7 +28,7 @@ install() {
   if [[ `uname` == Darwin ]]; then
     binary_type=darwin-amd64
     ytt_checksum=1f84e26457a5efd2470df408072bf2f182c939414e4958f82e19e122d2ee120c
-    kbld_checksum=d3b0a30bf3a79bedeb25d8548a91254954b99cd4a0c03f3a810b331fc4d1f071
+    kbld_checksum=5fc8a491327294717611974c6ab3da2bda3f3809ef3147c1e8472ac62af3ee18
     kapp_checksum=c5c7f34399293ccda62dc7b809535a8e2f2afb9901147429281c0f4884b13483
     kwt_checksum=555d50d5bed601c2e91f7444b3f44fdc424d721d7da72955725a97f3860e2517
     imgpkg_checksum=f0c87c8caefb3d2a82e648779b36783403fe5c93930df2d5cbf4968713933392
@@ -36,7 +36,7 @@ install() {
   else
     binary_type=linux-amd64
     ytt_checksum=2ca800c561464e0b252e5ee5cacff6aa53831e65e2fb9a09cf388d764013c40d
-    kbld_checksum=ba0be56d9e74b067f3e659de0b79100b0b9df86a2e3e0e6ff533b1e019c22c23
+    kbld_checksum=de546ac46599e981c20ad74cd2deedf2b0f52458885d00b46b759eddb917351a
     kapp_checksum=f8669039dfba001081c94576c898d10aba28ecceffcd98708e8f2c87c13109e4
     kwt_checksum=92a1f18be6a8dca15b7537f4cc666713b556630c20c9246b335931a9379196a0
     imgpkg_checksum=cfcfcb5afc5e3d28ce1f2f67971a4dcd18f514dadf8a63d70c864e49c9ddca7e
@@ -89,6 +89,7 @@ install() {
 }
 
 install
+
 
 
 
