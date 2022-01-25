c663a9ca12613fe763d3c8c31690e2f438ddcf85
rohitagg2020 <55523204+rohitagg2020@users.noreply.github.com>
2022-01-25 15:27:32 +0530

    Update install.sh

--- site/static/install.sh
+++ site/static/install.sh
@@ -20,7 +20,7 @@ install() {
 
   ytt_version=v0.38.0
   kbld_version=v0.32.0
-  kapp_version=v0.44.0
+  kapp_version=v0.45.0
   kwt_version=v0.0.6
   imgpkg_version=v0.24.0
   vendir_version=v0.23.0
@@ -29,7 +29,7 @@ install() {
     binary_type=darwin-amd64
     ytt_checksum=1f84e26457a5efd2470df408072bf2f182c939414e4958f82e19e122d2ee120c
     kbld_checksum=5fc8a491327294717611974c6ab3da2bda3f3809ef3147c1e8472ac62af3ee18
-    kapp_checksum=27980ca316c2124fb2e983ff971c9afb12be818ac93d7d63c6b03ddb42d38f97
+    kapp_checksum=44f66b7ed664e4537b2caa0453aa239f16e20c028b11fedc22f46f3cac3ba9fd
     kwt_checksum=555d50d5bed601c2e91f7444b3f44fdc424d721d7da72955725a97f3860e2517
     imgpkg_checksum=f0c87c8caefb3d2a82e648779b36783403fe5c93930df2d5cbf4968713933392
     vendir_checksum=aef3233128727c01ffac6561533c9f60a49307f41decc1baa18688a4a250b15f
@@ -37,7 +37,7 @@ install() {
     binary_type=linux-amd64
     ytt_checksum=2ca800c561464e0b252e5ee5cacff6aa53831e65e2fb9a09cf388d764013c40d
     kbld_checksum=de546ac46599e981c20ad74cd2deedf2b0f52458885d00b46b759eddb917351a
-    kapp_checksum=aaa5162b857868e64a270641b79a2a980e4528d40e88ea7741440b3028637c7c
+    kapp_checksum=9d2c0441729bebe49e5fb9a1a7b7088f0bee4092ba23e267339a9478948c5731
     kwt_checksum=92a1f18be6a8dca15b7537f4cc666713b556630c20c9246b335931a9379196a0
     imgpkg_checksum=cfcfcb5afc5e3d28ce1f2f67971a4dcd18f514dadf8a63d70c864e49c9ddca7e
     vendir_checksum=c6a65e7e8e589e25bf5554e9575ab247e55ae71920d3d366ffd84b1571fe34ac
