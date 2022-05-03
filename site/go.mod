d65b133cbbf27369cb1aa5a93b194eee94239acf
Cari <cdean@pivotal.io>
2022-05-03 15:15:30 -0700

    Test netilify provided example

---
+++ site/go.mod
@@ -0,0 +1,5 @@
+module example_internal_templating
+
+go 1.17
+
+require github.com/aws/aws-lambda-go v1.31.1
---
+++ site/playground-src/playground/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func Handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello World!",
+	}, nil
+}
+
+func main() {
+	// Initiate AWS Lambda handler
+	lambda.Start(Handler)
+}
